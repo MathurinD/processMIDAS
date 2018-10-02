4c8e839d0eec80a88890f011e7d882e7360ba8b9
Mathurin Dorel <dorel@horus.ens.fr>
2018-10-02 18:39:45 +0200

    Add merge and subset

---
+++ R/mergeMIDAS.R
@@ -0,0 +1,67 @@
+#' Merge several MIDAS together
+#'
+#' Merge MIDAS files together, adding NA to the fields that are not shared
+#' @param ... MIDAS filenames or matrices
+#' @param save_file Whether the resulting MIDAS matrix should be saved in a file
+#' @example mergeMIDAS("blunt1_MIDAS.csv", "blunt2_MIDAS.csv")
+#' @seealso extractMIDAS
+mergeMIDAS <- function(..., save_file=FALSE) {
+    mfiles = unlist(list(...))
+    all_ids = c()
+    all_measurements = c()
+    all_treatments = c()
+    midas_list = list()
+    # Collect all column names that should be in the merged file
+    for (ff in mfiles) {
+        midas_list[[ff]] = extractMIDAS(ff)
+        midas = midas_list[[ff]]
+        all_ids = unique(c(all_ids, colnames(midas)[grepl("^ID.", colnames(midas))]))
+        all_treatments = unique(c(all_treatments, colnames(midas)[grepl("^TR.", colnames(midas))]))
+        all_measurements = unique(c(all_measurements, colnames(midas)[grepl("^DV.", colnames(midas))]))
+    }
+    # Generate mergeable version of each MIDAS file
+    for (ff in mfiles) {
+        midas = midas_list[[ff]]
+        zero_col = rep(0, nrow(midas))
+        na_col = rep(NA, nrow(midas))
+        output = data.frame(row.names=1:nrow(midas))
+        for (id in all_ids) {
+            if (id %in% colnames(midas)) {
+                output = cbind(output, midas[,id])
+            } else {
+                output = cbind(output, na_col)
+            }
+        }
+        for (tt in all_treatments) {
+            if (tt %in% colnames(midas)) {
+                output = cbind(output, midas[,tt])
+            } else {
+                output = cbind(output, zero_col)
+            }
+        }
+        for (mm in all_measurements) {
+            if (mm %in% colnames(midas)) {
+                output = cbind(output, midas[,mm])
+            } else {
+                output = cbind(output, na_col)
+            }
+        }
+        colnames(output) = c(all_ids, all_treatments, all_measurements)
+        midas_list[[ff]] = output
+    }
+    save_midas = midas_list[[1]]
+    print(nrow(midas_list[[1]]))
+    for (midas in midas_list[-1]) {
+        print(nrow(midas))
+        save_midas = rbind(save_midas, midas)
+    }
+
+    save_dir = "merged" # TODO filter here too
+    dir.create(save_dir, FALSE)
+    save_name = paste0(save_dir, "/", "merged_", basename(mfiles[1])) # TODO extra filter for commun names
+    if (save_file) {
+        write.csv(save_midas, file="", quote=FALSE, row.names=FALSE)
+    }
+    return(save_midas)
+}
+
---
+++ R/subsetMIDAS.R
@@ -0,0 +1,37 @@
+#' Create subset of a MIDAS file
+#'
+#' Create new MIDAS files from a subset of lines of an existing MIDAS file
+#' @param midas_file A MIDAS matrix or filename
+#' @param subsets A vector of column name from the MIDAS 'TR:' fields. A '!' before the string means that the rows without this field will be included.
+#' @param save_file Whether the resulting MIDAS matrix should be saved in a file
+#' @examples subsetMIDAS("blunt_MIDAS.csv", c("parental", "shp2ko"))
+#' @export
+subsetMIDAS <- function(midas_file, subsets, save_file=FALSE) {
+    library(STASNet)
+    output_names = c()
+
+    midas_file = extractMIDAS(midas_file)
+    subsets_list = list()
+    for (item in subsets) {
+        choice = 1
+        if (substr(item, 1, 1) == "!") {
+            choice = 0
+            item = substr(item, 2, 1000)
+        }
+        selection = which(colnames(midas_file) == paste0("TR.", item))
+        blank_control = grepl("^(blank|c|control)$", midas_file[,"ID.type"])
+        save_midas = midas_file[blank_control | midas_file[,selection]==choice,-selection]
+
+        save_name = gsub("MIDAS", paste0(ifelse(choice==0, "no-", ""), item, "_MIDAS"), midas_file)
+        if (save_file) {
+            dir.create(item, showWarnings=FALSE)
+            save_name = paste0(item, "/", save_name)
+            output_names = c(output_names, save_name)
+            write.csv(save_midas, file=save_name, quote=FALSE, row.names=FALSE)
+        }
+        subsets_list[[item]] = save_midas
+    }
+    return(subsets_list)
+}
+
+
